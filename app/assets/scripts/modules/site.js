6f8460515790f778f921830cba952ebd5169aac6
johnmilner <john.milner@mac.com>
2018-04-15 15:25:12 -0700

    implement overlay menu logic for open and close pt.1

--- app/assets/scripts/modules/site.js
+++ app/assets/scripts/modules/site.js
@@ -1,21 +1,9 @@
-﻿﻿import S from 'skylake'
-
-// (function () {
-//   const tl = new S.Timeline()
-//   const isObj = S.Is.object(tl)
-//     tl.from({el: '#burger-menu-sail-l', p: {y: [0, 100, 'vh']}, d: 5000, e: 'linear'})
-//     tl.from({el: '#burger-menu-sail-r', p: {y: [0, 100, 'vh']}, d: 5000, e: 'linear', delay: 300})
-    
-//     tl.pause()
-    
-//   })();
+﻿import S from 'skylake'
 
   
   const burger = document.querySelector('#burger');
   burger.addEventListener('click', function() {
-    // const animation = new S.Merom({el: '#burger-menu-sail-l', p: {y: [0, -100, 'vh']}, d: 10000, e: 'Power4InOut'})
-    // animation.play()
-    // animation.play({p: {y: {newEnd: -100}}, reverse: true})
+    
     const tl = new S.Timeline()
     const isObj = S.Is.object(tl)
     tl.from({el: '#burger-menu-sail-l', p: {y: [0, 100]}, d: 2500, e: 'Power4InOut'})
@@ -23,15 +11,27 @@
     tl.from({el: '#burger-menu-list', p: {y: [0, 225]}, d: 2500, e: 'Power4InOut'})
     tl.from({el: '.burger-menu-link', p: {y: [-100, 0]}, d: 1600, e: 'ExpoOut', delay: 1800})
     tl.from({el: '.burger-menu-share', p: {y: [100, 0]}, d: 1600, e: 'ExpoOut', delay: 800})
+    
+    tl.from({el: '.burger-close', p: {y: [-108, 0]}, d: 1600, e: 'Power4InOut', delay: 400})
+    tl.from({el: '#burger-border-wrap', p: {opacity: [0, .5]}, d: 1600, e: 'Power4InOut'})
 
-    burger.classList.add('active')
+    burger.className = 'oh active';
 
-    //tl.from('.burger-menu-link', p: {y: [0, 0]}, 1600, "ExpoOut", { delay: 800 })
-    //tl.from({el: '#burger-menu-link-0', p: {y: [0, 0]}, d: 2500, e: 'Power4InOut', delay: 3350})
 
     tl.play()
     
     console.log('clicked!');
+    if (burger.className = 'oh active') {
+      burger.addEventListener('click', function() {
+
+        tl.play({reverse: true})
+        burger.className = 'oh'
+
+  
+      })
+    } 
   });
+
+ 
 
   
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10798,38 +10798,32 @@ var _skylake2 = _interopRequireDefault(_skylake);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// (function () {
-//   const tl = new S.Timeline()
-//   const isObj = S.Is.object(tl)
-//     tl.from({el: '#burger-menu-sail-l', p: {y: [0, 100, 'vh']}, d: 5000, e: 'linear'})
-//     tl.from({el: '#burger-menu-sail-r', p: {y: [0, 100, 'vh']}, d: 5000, e: 'linear', delay: 300})
-
-//     tl.pause()
-
-//   })();
-
-
 var burger = document.querySelector('#burger');
 burger.addEventListener('click', function () {
-    // const animation = new S.Merom({el: '#burger-menu-sail-l', p: {y: [0, -100, 'vh']}, d: 10000, e: 'Power4InOut'})
-    // animation.play()
-    // animation.play({p: {y: {newEnd: -100}}, reverse: true})
-    var tl = new _skylake2.default.Timeline();
-    var isObj = _skylake2.default.Is.object(tl);
-    tl.from({ el: '#burger-menu-sail-l', p: { y: [0, 100] }, d: 2500, e: 'Power4InOut' });
-    tl.from({ el: '#burger-menu-sail-r', p: { y: [0, 100] }, d: 2500, e: 'Power4InOut', delay: 50 });
-    tl.from({ el: '#burger-menu-list', p: { y: [0, 225] }, d: 2500, e: 'Power4InOut' });
-    tl.from({ el: '.burger-menu-link', p: { y: [-100, 0] }, d: 1600, e: 'ExpoOut', delay: 1800 });
-    tl.from({ el: '.burger-menu-share', p: { y: [100, 0] }, d: 1600, e: 'ExpoOut', delay: 800 });
 
-    burger.classList.add('active');
+  var tl = new _skylake2.default.Timeline();
+  var isObj = _skylake2.default.Is.object(tl);
+  tl.from({ el: '#burger-menu-sail-l', p: { y: [0, 100] }, d: 2500, e: 'Power4InOut' });
+  tl.from({ el: '#burger-menu-sail-r', p: { y: [0, 100] }, d: 2500, e: 'Power4InOut', delay: 50 });
+  tl.from({ el: '#burger-menu-list', p: { y: [0, 225] }, d: 2500, e: 'Power4InOut' });
+  tl.from({ el: '.burger-menu-link', p: { y: [-100, 0] }, d: 1600, e: 'ExpoOut', delay: 1800 });
+  tl.from({ el: '.burger-menu-share', p: { y: [100, 0] }, d: 1600, e: 'ExpoOut', delay: 800 });
 
-    //tl.from('.burger-menu-link', p: {y: [0, 0]}, 1600, "ExpoOut", { delay: 800 })
-    //tl.from({el: '#burger-menu-link-0', p: {y: [0, 0]}, d: 2500, e: 'Power4InOut', delay: 3350})
+  tl.from({ el: '.burger-close', p: { y: [-108, 0] }, d: 1600, e: 'Power4InOut', delay: 400 });
+  tl.from({ el: '#burger-border-wrap', p: { opacity: [0, .5] }, d: 1600, e: 'Power4InOut' });
 
-    tl.play();
+  burger.className = 'oh active';
 
-    console.log('clicked!');
+  tl.play();
+
+  console.log('clicked!');
+  if (burger.className = 'oh active') {
+    burger.addEventListener('click', function () {
+
+      tl.play({ reverse: true });
+      burger.className = 'oh';
+    });
+  }
 });
 
 /***/ })
