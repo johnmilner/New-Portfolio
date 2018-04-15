4c40c54b8c581580e158d9c80e0fee4564e47366
johnmilner <john.milner@mac.com>
2018-04-14 22:03:52 -0700

    implement menuoverlay with text fade/slidein animations and adjust font url paths in site.css

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10816,8 +10816,17 @@ burger.addEventListener('click', function () {
     // animation.play({p: {y: {newEnd: -100}}, reverse: true})
     var tl = new _skylake2.default.Timeline();
     var isObj = _skylake2.default.Is.object(tl);
-    tl.from({ el: '#burger-menu-sail-l', p: { y: [0, 100, 'vh'] }, d: 2500, e: 'Power4InOut' });
-    tl.from({ el: '#burger-menu-sail-r', p: { y: [0, 100, 'vh'] }, d: 2500, e: 'Power4InOut', delay: 50 });
+    tl.from({ el: '#burger-menu-sail-l', p: { y: [0, 100] }, d: 2500, e: 'Power4InOut' });
+    tl.from({ el: '#burger-menu-sail-r', p: { y: [0, 100] }, d: 2500, e: 'Power4InOut', delay: 50 });
+    tl.from({ el: '#burger-menu-list', p: { y: [0, 225] }, d: 2500, e: 'Power4InOut' });
+    tl.from({ el: '.burger-menu-link', p: { y: [-100, 0] }, d: 1600, e: 'ExpoOut', delay: 1800 });
+    tl.from({ el: '.burger-menu-share', p: { y: [100, 0] }, d: 1600, e: 'ExpoOut', delay: 800 });
+
+    burger.classList.add('active');
+
+    //tl.from('.burger-menu-link', p: {y: [0, 0]}, 1600, "ExpoOut", { delay: 800 })
+    //tl.from({el: '#burger-menu-link-0', p: {y: [0, 0]}, d: 2500, e: 'Power4InOut', delay: 3350})
+
     tl.play();
 
     console.log('clicked!');
